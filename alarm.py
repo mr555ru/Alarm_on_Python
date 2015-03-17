4d2b0bcbec0aa03a1fd55ac926e7b5f36f79d09a
mr555 <rutroll@gmail.com>
2015-03-17 08:36:40 +0300

    воспроизведение звука через pygame

--- alarm.py
+++ alarm.py
@@ -1,16 +1,18 @@
 # -*- coding: utf-8 -*-
 from Tkinter import *
 import datetime
-import pyglet
+import pygame
 root = Tk()
+
+pygame.init()
+sound = pygame.mixer.Sound("mlg.wav")
 
 def Time(event):
     lbl["text"] = datetime.datetime.now().time().strftime("%H:%M:%S") # https://docs.python.org/2/library/datetime.html#strftime-strptime-behavior
 
 def AlarmSound(event):
-    sound = pyglet.resource.media('mlg.mp3')
+    #sound = pyglet.resource.media('mlg.mp3')
     sound.play()
-
     
 lbl = Label(root, text = " ", width=30,height=2, bg = "black", fg = "white")
 lbl.pack()
